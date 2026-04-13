b04b3b85038d6f7ea112fc56cd5b19d1cd5e9aa9
awassmer <s253740@dtu.dk>
2026-04-13 20:28:42 +0200

    Updated huggingface cli commands to hf

--- setup_and_download.sh
+++ setup_and_download.sh
@@ -9,7 +9,7 @@ echo "========================================="
 
 # 1. Setup Conda Environment
 echo "--> Initializing Miniforge..."
-source ~/miniforge3/bin/activate
+source $BLACKHOLE/miniforge3/bin/activate
 
 # check if conda env 'adlcv' exists
 if ! conda env list | grep -q "^adlcv "; then
@@ -38,16 +38,16 @@ export HF_HOME="./models" # Forces models to download here instead of ~/.cache
 
 # VLM
 echo "Downloading Qwen2.5-VL-3B-Instruct..."
-huggingface-cli download Qwen/Qwen2.5-VL-3B-Instruct --local-dir ./models/Qwen2.5-VL-3B-Instruct
+hf download Qwen/Qwen2.5-VL-3B-Instruct --local-dir ./models/Qwen2.5-VL-3B-Instruct
 
 
 # LLM as a judge
 echo "Downloading Prometheus-2-7B..."
-huggingface-cli download prometheus-eval/prometheus-7b-v2.0 --local-dir ./models/Prometheus-2-7B
+hf download prometheus-eval/prometheus-7b-v2.0 --local-dir ./models/Prometheus-2-7B
 
 # Patronus-Lynx Model
-#echo "Downloading Patronus-Lynx-8B-v1.1..."
-#huggingface-cli download PatronusAI/Llama-3-Patronus-Lynx-8B-Instruct-v1.1 --local-dir ./models/Patronus-Lynx-8B
+echo "Downloading Patronus-Lynx-8B-v1.1..."
+hf download PatronusAI/Llama-3-Patronus-Lynx-8B-Instruct-v1.1 --local-dir ./models/Patronus-Lynx-8B
 
 # Datasets
 echo "--> Downloading VQA v2 Annotations and Questions..."
